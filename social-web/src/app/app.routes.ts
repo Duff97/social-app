11e7d155ff1bbee09d5bbd91c6b369e0141c847e
Duff97 <felduf12@gmail.com>
2024-08-29 19:23:42 -0400

    sign-in component generation and route setup

--- social-web/src/app/app.routes.ts
+++ social-web/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'sign-in',
+    pathMatch: 'full'
+  },
+  {
+    path: 'sign-in',
+    loadComponent: () => import('./sign-in/sign-in.component').then(m => m.SignInComponent)
+  },
+];
